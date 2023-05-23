8e1ce616cdf6fd8888333f07de75ef5cfae1fd62
chaimaeboukili <cboukili@hotmail.fr>
2023-05-23 15:08:31 -0700

    execute command

--- execute_cmd.c
+++ execute_cmd.c
@@ -8,24 +8,24 @@
 
 void execute_cmd(char **argvv)
 {
-int pid = 0;
-  
-if (!argvv || !argvv[0])
-return;
-pid = fork();
-if (pid == 0)
-{
-if (execve(argvv[0], argvv, NULL) == -1)
-{
-perror("hsh");
-exit(0);
-}
-}
-else if (pid == -1)
-{
-perror("hsh error - child is -1");
-exit(-1);
-}
-else
-wait(NULL);
+	int pid = 0;
+
+	if (!argvv || !argvv[0])
+	return;
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve(argvv[0], argvv, NULL) == -1)
+		{
+		perror("hsh");
+		exit(0);
+		}
+	}
+	else if (pid == -1)
+	{
+		perror("hsh error - child is -1");
+		exit(-1);
+	}
+	else
+	wait(NULL);
 }
