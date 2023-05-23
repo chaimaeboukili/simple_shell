9b5fa8b0296a02a32b99762b9710815e67e79d4b
chaimaeboukili <126779992+chaimaeboukili@users.noreply.github.com>
2023-05-23 21:32:03 +0200

    execute command

---
+++ execute_cmd.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+/**
+* execute_cmd - execute command
+* @argvv: array of strings that needs to be checked
+*Return: void
+*/
+
+void execute_cmd(char **argvv)
+{
+int pid = 0;
+if (!argvv || !argvv[0])
+return;
+pid = fork();
+if (pid == 0)
+{
+if (execve(argvv[0], argvv, NULL) == -1)
+{
+perror("hsh");
+exit(0);
+}
+}
+else if (pid == -1)
+{
+perror("hsh error - child is -1");
+exit(-1);
+}
+else
+wait(NULL);
+}
