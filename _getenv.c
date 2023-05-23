034e45cbf134e580405f33d13d6f9f66c4f6d193
chaimaeboukili <126779992+chaimaeboukili@users.noreply.github.com>
2023-05-23 22:55:22 +0200

    environment variable

---
+++ _getenv.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include "shell.h"
+
+/**
+* _getenv - to get an environment variable
+* @env: the environment in args
+* Return: pointer to char
+*/
+
+char *_getenv(char *env)
+{
+int i, j;
+char *val;
+
+if (!env)
+return (0);
+for (i = 0; environ[i]; i++)
+{
+j = 0;
+if (env[j] == environ[i][j])
+{
+while (env[j])
+{
+if (env[j] != environ[i][j])
+break;
+j++;
+}
+if (env[j] == '\0')
+{
+val = (environ[i] + j + 1);
+return (val);
+}
+}
+}
+return (0);
+}
