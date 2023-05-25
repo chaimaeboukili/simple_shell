999bd8d32309d4eda853d8f3d3525740bd2358c8
chaimaeboukili <126779992+chaimaeboukili@users.noreply.github.com>
2023-05-25 03:16:46 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -19,7 +19,7 @@ int _atoi(char *s);
 char *_read_string();
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-
+char *_strcat(char *dest, char *src);
 size_t _strncmp(char *s1, char *s2, size_t n);
 int _help(char **args);
 int _ctrld(char **args);
