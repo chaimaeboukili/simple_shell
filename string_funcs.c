80ca9148f7ad9e0b621ef942d62e5691fa67decd
chaimaeboukili <126779992+chaimaeboukili@users.noreply.github.com>
2023-05-25 03:17:30 +0200

    Update string_funcs.c

--- string_funcs.c
+++ string_funcs.c
@@ -2,6 +2,30 @@
 #include "shell.h"
 
 /**
+ *_strncmp -  function that compares two strings.
+ *@s1: string one
+ *@s2: string two
+ *@n: number of characters
+ * Return: diference
+ */
+
+size_t _strncmp(char *s1, char *s2, size_t n)
+{
+	size_t i, j;
+
+	for (j = 0; s1[j] != '\0' && j < n; j++)
+	{
+		i = s1[j] - s2[j];
+
+		if (i != 0)
+		{
+			return (i);
+		}
+	}
+	return (0);
+}
+
+/**
  * _strcat - cantenates two strings
  *
  * @dest: Destination string
