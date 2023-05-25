9012dfb5fe7963112f3ff2f64fbcd30a8b842290
chaimaeboukili <126779992+chaimaeboukili@users.noreply.github.com>
2023-05-25 03:16:23 +0200

    Update string_funcs.c

--- string_funcs.c
+++ string_funcs.c
@@ -2,6 +2,31 @@
 #include "shell.h"
 
 /**
+ * _strcat - cantenates two strings
+ *
+ * @dest: Destination string
+ *
+ * @src: Origin string
+ * Return: Always 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+
+	}
+	dest[i] = '\0';
+return (dest);
+}
+/**
  * _strlen - calculate the length of a string
  * @str: the string
  * Return: the calculated length
