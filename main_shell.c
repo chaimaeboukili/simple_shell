d7702ae556a28949150ada9830a883e558b5eb5e
chaimaeboukili <126779992+chaimaeboukili@users.noreply.github.com>
2023-05-25 02:04:19 +0200

    Update main_shell.c

--- main_shell.c
+++ main_shell.c
@@ -9,25 +9,25 @@
 int main(int ac, char **av, char **env)
 {
 	int pathValue = 0, status = 0, is_path = 0;
-	char *line = NULL, /**ptr to inpt*/ **commands = NULL; /**tokenized commands*/
+	char *line = NULL,  **commands = NULL;
 	(void)ac;
-	while (1)/* loop until exit */
+	while (1)
 	{
 		errno = 0;
-		line = _getline_command();/** reads user input*/
+		line = _getline_command();
 		if (line == NULL && errno == 0)
 			return (0);
 		if (line)
 		{
 			pathValue++;
-			commands = tokenize(line);/** tokenizes or parse user input*/
+			commands = tokenize(line);
 			if (!commands)
 				free(line);
-			if (!_strcmp(commands[0], "env"))/**checks if user wrote env"*/
+			if (!_strcmp(commands[0], "env"))
 				_getenv(env);
 			else
 			{
-				is_path = _values_path(&commands[0], env);/** tokenizes PATH*/
+				is_path = values_of_path(&commands[0], env);
 				status = _fork_fun(commands, av, env, line, pathValue, is_path);
 					if (status == 200)
 					{
@@ -37,12 +37,12 @@ int main(int ac, char **av, char **env)
 				if (is_path == 0)
 					free(commands[0]);
 			}
-			free(commands); /*free up memory*/
+			free(commands); 
 		}
 		else
 		{
 			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);/** Writes to standard output*/
+				write(STDOUT_FILENO, "\n", 1);
 			exit(status);
 		}
 		free(line);
