a7314e458b29764aabe433980f4dca2531000a75
chaimaeboukili <126779992+chaimaeboukili@users.noreply.github.com>
2023-05-25 02:11:05 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -23,7 +23,7 @@ int _atoi(char *s);
 int _strlen(char *s);
 char *read_string();
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *_getenv(char *env);
+
 
 
 int _help(char **args);
@@ -31,7 +31,7 @@ int _ctrld(char **args);
 char *builtin_str[] = {"help", "^D"};
 int _num_builtins(void);
 int _fork_fun(char **arg, char **av, char **env, char *lineptr, int np, int c);
-char *find_path(char *av);
+char *find_path(char **env);
 char _getenv(char **env);
 char *_strcpy(char *dest, char *src);
 char *concat_all(char *str1, char *str2);
